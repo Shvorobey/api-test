18f384e049ab8da5f76e31f28fe4813d0a8fb1fb
Shvorobey <a.shvorobey@gmail.com>
2019-05-24 13:40:17 +0300

    Создан проект по АПИ

--- routes/api.php
+++ routes/api.php
@@ -95,3 +95,23 @@ Route::put('/clients/{id}', function (Request $request, $id) {
     $client->save();
     return response()->json($client, 201);
 });
+
+//Создание проекта
+Route::post('/projects', function (Request $request) {
+    $rules = [
+        'name' => 'required|max:150|min:2',
+        'description' => 'required|max:35|min:2',
+        'statuses' => 'required|in:planned, running, on hold, finished, cancel',
+    ];
+    /** @var \Illuminate\Support\Facades\Validator */
+    $validator = \Illuminate\Support\Facades\Validator::make($request->all(), $rules);
+    if ($validator->fails()) {
+        return response()->json($validator->errors(), 400);
+    }
+    $project = new \App\project();
+    $project->name = $request->post('name');
+    $project->description = $request->post('description');
+    $project->statuses = $request->post('statuses');
+    $project->save();
+    return response()->json($project, 201);
+});
